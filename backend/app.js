915c5ee97234d84526bba86290c45ebc877d1808
chocolatl <choclatl@outlook.com>
2018-02-11 13:24:00 +0800

    错误处理中间件返回json格式响应

--- backend/app.js
+++ backend/app.js
@@ -37,9 +37,11 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    errMsg: res.locals.message,
+    error: res.locals.error
+  });
 });
 
 module.exports = app;
