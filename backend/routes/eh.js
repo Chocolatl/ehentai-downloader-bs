ead8972691a645c5b07a5d4be288006212b76475
chocolatl <choclatl@outlook.com>
2018-04-27 12:34:20 +0800

    添加对里站的搜索支持

--- backend/routes/eh.js
+++ backend/routes/eh.js
@@ -1,13 +1,23 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const request = require('request');
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
+const yaml = require('js-yaml');
+const CONFIG_FILE = require('path').normalize(`${__dirname}/../config.yml`);
+const USER_CONFIG = yaml.load(fs.readFileSync(CONFIG_FILE, 'utf8'));
 
 router.get('/search', function (req, res, next) {
+	let isLoginEx = !!USER_CONFIG['login']['igneous'];
 	let keywords = req.query.keywords;
 	let page = req.query.page || '0';
-	let searchUrl = (keywords) => `https://e-hentai.org/?page=${page}&f_doujinshi=1&f_manga=1&f_artistcg=1&f_gamecg=1&f_western=1&f_non-h=1&f_imageset=1&f_cosplay=1&f_asianporn=1&f_misc=1&f_search=${encodeURIComponent(keywords)}&f_apply=Apply+Filter`;
-	request.get(searchUrl(keywords), (err, response, body) => {
+	let searchUrl = (keywords, ex = false) => `https://e${ex ? 'x' : '-'}hentai.org/?page=${page}&f_doujinshi=1&f_manga=1&f_artistcg=1&f_gamecg=1&f_western=1&f_non-h=1&f_imageset=1&f_cosplay=1&f_asianporn=1&f_misc=1&f_search=${encodeURIComponent(keywords)}&f_apply=Apply+Filter`;
+	request.get({
+		url: searchUrl(keywords, isLoginEx),
+		headers: {
+			Cookie: Object.entries(USER_CONFIG['login']).map(i => i.join('=')).join('; ')
+		}
+	}, (err, response, body) => {
 		const { document } = (new JSDOM(body)).window;
 		let items = Array.from(document.querySelectorAll('.gtr0, .gtr1'));
 		let pageLinks = document.querySelectorAll('.ptt a');
